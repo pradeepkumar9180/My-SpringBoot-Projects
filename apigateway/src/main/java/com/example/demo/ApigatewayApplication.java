d4053ac36e6030b334fb51e5a51624957b64dae0
pradeepkumar9180 <82466665+pradeepkumar9180@users.noreply.github.com>
2023-08-05 13:00:17 +0530

    Add files via upload

---
+++ apigateway/src/main/java/com/example/demo/ApigatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
